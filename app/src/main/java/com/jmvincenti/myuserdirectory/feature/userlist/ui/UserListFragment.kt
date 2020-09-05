d5cbeec53a0b888c964faa2e22b047da8d780d64
JM Vincenti <jean.marie.vincenti@gmail.com>
2020-09-05 17:47:11 +0200

    Create empty UserList Fragment/ViewModel

---
+++ app/src/main/java/com/jmvincenti/myuserdirectory/feature/userlist/ui/UserListFragment.kt
@@ -0,0 +1,31 @@
+package com.jmvincenti.myuserdirectory.feature.userlist.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.jmvincenti.myuserdirectory.R
+
+class UserListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = UserListFragment()
+    }
+
+    private lateinit var viewModel: UserListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.user_list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(UserListViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+}
---
+++ app/src/main/java/com/jmvincenti/myuserdirectory/feature/userlist/ui/UserListViewModel.kt
@@ -0,0 +1,7 @@
+package com.jmvincenti.myuserdirectory.feature.userlist.ui
+
+import androidx.lifecycle.ViewModel
+
+class UserListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
