14417c058c253b04734ff3af400861070c6f8c22
JM Vincenti <jean.marie.vincenti@gmail.com>
2020-09-05 16:24:14 +0200

    Update UserListStore with LoadMore/LoadInitial reducer & middleware

--- app/src/main/java/com/jmvincenti/myuserdirectory/feature/userlist/store/UserListStore.kt
+++ app/src/main/java/com/jmvincenti/myuserdirectory/feature/userlist/store/UserListStore.kt
@@ -1,19 +1,29 @@
 package com.jmvincenti.myuserdirectory.feature.userlist.store
 
+import com.jmvincenti.myuserdirectory.feature.userlist.domain.UserListUseCase
 import com.jmvincenti.myuserdirectory.feature.userlist.model.UserListAction
 import com.jmvincenti.myuserdirectory.feature.userlist.model.UserListState
 import com.jmvincenti.statemachine.BaseStoreImpl
 import com.jmvincenti.statemachine.Middleware
 import com.jmvincenti.statemachine.Reducer
 import com.jmvincenti.statemachine.SimpleLoadingState
+import kotlinx.coroutines.CoroutineScope
 import java.util.concurrent.Executor
 
 class UserListStore(
+    scope: CoroutineScope,
+    useCase: UserListUseCase,
     override val executor: Executor,
     override val reducers: List<Reducer<UserListState, UserListAction>> =
-        emptyList(),
+        listOf(
+            initialReducer,
+            moreReducer
+        ),
     override val middlewares: List<Middleware<UserListState, UserListAction>> =
-        emptyList()
+        listOf(
+            LoadInitialMiddleWare(scope, useCase),
+            LoadMoreMiddleWare(scope, useCase)
+        )
 ) : BaseStoreImpl<UserListState, UserListAction>(
     initialState = UserListState(
         currentPage = 0,
