91a623a8f799d0b75df208e1a3db0d9e1c80114a
JM Vincenti <jean.marie.vincenti@gmail.com>
2020-09-05 17:43:20 +0200

    Implement UserListRemoteDataSource using UserApiClient

---
+++ app/src/main/java/com/jmvincenti/myuserdirectory/mapper/ApiUserMapper.kt
@@ -0,0 +1,26 @@
+package com.jmvincenti.myuserdirectory.mapper
+
+import com.jmvincenti.myuserdirectory.apiclient.model.ApiLocation
+import com.jmvincenti.myuserdirectory.apiclient.model.ApiUser
+import com.jmvincenti.myuserdirectory.model.Location
+import com.jmvincenti.myuserdirectory.model.Pictures
+import com.jmvincenti.myuserdirectory.model.User
+
+fun ApiUser.toModel(fullNameBuilder: UserFullNameBuilder): User = User(
+    id = id.value,
+    fullName = fullNameBuilder.build(this),
+    email = email,
+    phone = phone,
+    pictures = Pictures(
+        thumbnail = picture.thumbnail,
+        cover = picture.large
+    ),
+    location = location.toModel()
+)
+
+fun ApiLocation.toModel(): Location = Location(
+    street = street,
+    state = state,
+    city = city,
+    postcode = postcode
+)
