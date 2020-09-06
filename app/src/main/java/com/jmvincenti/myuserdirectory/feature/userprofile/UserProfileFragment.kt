0a4f13ccbb9b9555826318147f1f0b151277c189
JM Vincenti <jean.marie.vincenti@gmail.com>
2020-09-06 11:06:16 +0200

    Add userId to UserProfileFragment parameter

--- app/src/main/java/com/jmvincenti/myuserdirectory/feature/userprofile/UserProfileFragment.kt
+++ app/src/main/java/com/jmvincenti/myuserdirectory/feature/userprofile/UserProfileFragment.kt
@@ -6,28 +6,50 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.jmvincenti.myuserdirectory.R
+import com.jmvincenti.myuserdirectory.databinding.UserProfileFragmentBinding
+import com.jmvincenti.myuserdirectory.feature.userprofile.model.UserProfileCommand
 import com.jmvincenti.myuserdirectory.feature.userprofile.model.UserProfileState
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+
+private const val PARAM_USER_ID = "Param:userId"
 
 @AndroidEntryPoint
 class UserProfileFragment : Fragment() {
 
     companion object {
-        fun newInstance() = UserProfileFragment()
+        fun newInstance(userId: String): UserProfileFragment {
+            val fragment = UserProfileFragment()
+
+            val args = Bundle()
+            args.putString(PARAM_USER_ID, userId)
+            fragment.arguments = args
+
+            return fragment
+        }
     }
+
+    private lateinit var userId: String
 
     @Inject
     lateinit var factory: UserProfileViewModel.Factory
     private lateinit var viewModel: UserProfileViewModel
 
+    private var _binding: UserProfileFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        userId = requireNotNull(arguments?.getString(PARAM_USER_ID))
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.user_profile_fragment, container, false)
-    }
+    ): View? = UserProfileFragmentBinding.inflate(inflater, container, false)
+        .also { _binding = it }
+        .root
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -35,9 +57,15 @@ class UserProfileFragment : Fragment() {
             .get(UserProfileViewModel::class.java)
 
         viewModel.state().observe(viewLifecycleOwner, this@UserProfileFragment::bind)
+        viewModel.onCommand(UserProfileCommand.Init(userId))
     }
 
     private fun bind(state: UserProfileState) {
         TODO("Not yet implemented")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
