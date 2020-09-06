9d9532653cd7c7e769b66404ac7eade65ceeb7e3
JM Vincenti <jean.marie.vincenti@gmail.com>
2020-09-06 10:46:44 +0200

    Create empty UserProfileFragment and ViewModel

---
+++ app/src/main/java/com/jmvincenti/myuserdirectory/feature/userprofile/UserProfileFragment.kt
@@ -0,0 +1,31 @@
+package com.jmvincenti.myuserdirectory.feature.userprofile
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jmvincenti.myuserdirectory.R
+
+class UserProfileFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = UserProfileFragment()
+    }
+
+    private lateinit var viewModel: UserProfileViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.user_profile_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(UserProfileViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+}
---
+++ app/src/main/java/com/jmvincenti/myuserdirectory/feature/userprofile/UserProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.jmvincenti.myuserdirectory.feature.userprofile
+
+import androidx.lifecycle.ViewModel
+
+class UserProfileViewModel() : ViewModel() {
+    // TODO: Implement the ViewModel
+}
